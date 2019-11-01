5a7f69fe84ecb715f014307c8bca3d98ff66f080
bulletmys <bulletmys@yandex.ru>
2019-11-02 01:21:26 +0300

    Scripted auto-commit on change (2019-11-02 01:21:26) by gitwatch.sh

--- src/main/java/FlightsMapper.java
+++ src/main/java/FlightsMapper.java
@@ -5,14 +5,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightsMapper extends Mapper<LongWritable, Text, KeyWritableComparable, Text> {
-    private static int AIRPORT_ID = 14;
-    private static int AIRPORT_DELAY = 1;
-    private static byte AIRPORT_INDICATOR = 0;
+    private static int DEST_ID = 14;
+    private static int FLIGHT_DELAY = 18;
+    private static byte FLIGHT_INDICATOR = 1;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] strings = new CsvReader(value).getStrings();
 
-        context.write(new KeyWritableComparable(Integer.parseInt(strings[AIRPORT_ID]), AIRPORT_INDICATOR), new Text(strings[AIRPORT_NAME]));
+        if (Integer.parseInt(strings[FLIGHT_DELAY]) != 0) {
+            context.write(new KeyWritableComparable(Integer.parseInt(strings[DEST_ID]), FLIGHT_INDICATOR), new Text(strings[FLIGHT_DELAY]));
+        }
     }
 }
