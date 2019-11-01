61bed460ebeb5e6c556906f239cc38b1ed154f73
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:16:23 +0300

    Scripted auto-commit on change (2019-11-02 02:16:23) by gitwatch.sh

--- src/main/java/AirportIdGroupComparator.java
+++ src/main/java/AirportIdGroupComparator.java
@@ -6,12 +6,13 @@ import java.util.Comparator;
 public class AirportIdGroupComparator implements RawComparator<KeyWritableComparable> {
 
     @Override
-    public int compare(KeyWritableComparable keyWritableComparable, KeyWritableComparable t1) {
+    public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
         return 0;
     }
 
     @Override
-    public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
-        return 0;
+    public int compare(KeyWritableComparable obj1, KeyWritableComparable obj2) {
+        return Integer.compare(obj1.getAirportId(), obj2.getAirportId());
     }
+
 }
