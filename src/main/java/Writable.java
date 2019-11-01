25c5a1c17dd7d73c41336588fc68b2e9b79dfc32
bulletmys <bulletmys@yandex.ru>
2019-11-01 23:56:34 +0300

    Scripted auto-commit on change (2019-11-01 23:56:34) by gitwatch.sh

--- src/main/java/Writable.java
+++ src/main/java/Writable.java
@@ -1,2 +1,18 @@
-public class Writable implements org.apache.hadoop.io.Writable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class CsvWritable implements Writable {
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
