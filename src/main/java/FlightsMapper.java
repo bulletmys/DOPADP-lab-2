5c4e5227c632145673f4643086cfe5b41f2906c9
bulletmys <bulletmys@yandex.ru>
2019-11-02 01:16:33 +0300

    Scripted auto-commit on change (2019-11-02 01:16:33) by gitwatch.sh

--- src/main/java/FlightsMapper.java
+++ src/main/java/FlightsMapper.java
@@ -1,2 +1,14 @@
-public class FlightsMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightsMapper extends Mapper<LongWritable, Text, KeyWritableComparable, Text> {
+
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+    }
 }
