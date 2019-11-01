3fe31a0c6732ec96696d1dd68643ac8f2e35bae4
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:27:02 +0300

    Scripted auto-commit on change (2019-11-02 02:27:02) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -25,6 +25,6 @@ public class JoinReducer extends Reducer<KeyWritableComparable, Text, Text, Text
             }
         }
 
-        context.write();
+        context.write(airportName, );
     }
 }
