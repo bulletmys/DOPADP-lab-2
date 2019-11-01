6aea9af16fb534f8548dcfb2626021291584dfaf
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:23:31 +0300

    Scripted auto-commit on change (2019-11-02 02:23:31) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 public class JoinReducer extends Reducer<KeyWritableComparable, Text, Text, Text> {
     @Override
     protected void reduce(KeyWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        Text airportName = values.
+        Text airportName = values.iterator().next();
+        Itrea
+        while ()
     }
 }
