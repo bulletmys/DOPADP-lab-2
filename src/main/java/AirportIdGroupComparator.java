1f1e0f4344c3bf38a54484bde7fc948f4f5d8773
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:14:08 +0300

    Scripted auto-commit on change (2019-11-02 02:14:08) by gitwatch.sh

--- src/main/java/AirportIdGroupComparator.java
+++ src/main/java/AirportIdGroupComparator.java
@@ -3,7 +3,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 import java.util.Comparator;
 
-public class AirportIdGroupComparator implements RawComparator {
+public class AirportIdGroupComparator implements RawComparator<KeyWritableComparable> {
 
     @Override
     public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
@@ -12,6 +12,11 @@ public class AirportIdGroupComparator implements RawComparator {
 
     @Override
     public int compare(Object o, Object t1) {
+        return 0;
+    }
+
+    @Override
+    public int compare(KeyWritableComparable keyWritableComparable, KeyWritableComparable t1) {
         return 0;
     }
 }
