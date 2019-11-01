3a5c49bc31dbf34825056c847ee04c408cdeb43e
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:42:38 +0300

    Scripted auto-commit on change (2019-11-02 02:42:38) by gitwatch.sh

--- src/main/java/ReduceSideJoin.java
+++ src/main/java/ReduceSideJoin.java
@@ -25,6 +25,6 @@ public class ReduceSideJoin {
 
         job.setNumReduceTasks(2);
 
-        System.exit(job.waitForCompletion(true) ? );
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
