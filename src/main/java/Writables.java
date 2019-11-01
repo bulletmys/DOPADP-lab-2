60d05a20fa285132120e42d46a4de16a4547f5ef
bulletmys <bulletmys@yandex.ru>
2019-11-01 23:40:47 +0300

    Scripted auto-commit on change (2019-11-01 23:40:47) by gitwatch.sh

--- src/main/java/Writables.java
+++ src/main/java/Writables.java
@@ -1,4 +1,22 @@
 import org.apache.hadoop.io.WritableComparable;
 
-public class WritableComparable extends WritableComparable<> {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class KeyWritableComparable extends WritableComparable<KeyWritableComparable> {
+    @Override
+    public int compareTo(KeyWritableComparable keyWritableComparable) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
