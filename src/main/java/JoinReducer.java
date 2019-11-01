444de41b8c418d35e47911d3da1202c4d38f52e7
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:18:52 +0300

    Scripted auto-commit on change (2019-11-02 02:18:52) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class JoinReducer extends Reducer<KeyWritableComparable, Text, > {
+public class JoinReducer extends Reducer<KeyWritableComparable, Text, Text, Text> {
+    
 }
