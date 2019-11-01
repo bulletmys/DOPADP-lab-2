81cf47d5ca29be72599c46e200fc15c4a885fbd4
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:01:15 +0300

    Scripted auto-commit on change (2019-11-02 02:01:15) by gitwatch.sh

--- src/main/java/AirportIdPartitioner.java
+++ src/main/java/AirportIdPartitioner.java
@@ -1,5 +1,9 @@
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class AirportIdPartitioner extends Partitioner<> {
+public class AirportIdPartitioner extends Partitioner<KeyWritableComparable, Text> {
 
+    @Override
+    public int getPartition(KeyWritableComparable keyWritableComparable, Text text, int i) {
+        return 0;
+    }
 }
