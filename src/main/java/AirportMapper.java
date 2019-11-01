f7e9dc77e5956c29c1422258dedd6159be7efe7e
bulletmys <bulletmys@yandex.ru>
2019-11-02 00:50:53 +0300

    Scripted auto-commit on change (2019-11-02 00:50:53) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -2,9 +2,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 
 public class AirportMapper extends Mapper<LongWritable, Text, KeyWritableComparable, Text>{
 
-    
-
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        
+    }
 }
