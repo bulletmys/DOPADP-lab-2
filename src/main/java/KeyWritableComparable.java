ab1bc6e219d5d85be42045a477b7219a16d4bd28
bulletmys <bulletmys@yandex.ru>
2019-11-01 23:47:46 +0300

    Scripted auto-commit on change (2019-11-01 23:47:46) by gitwatch.sh

--- src/main/java/KeyWritableComparable.java
+++ src/main/java/KeyWritableComparable.java
@@ -5,11 +5,16 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class KeyWritableComparable implements WritableComparable<KeyWritableComparable> {
-    int airportId;
-    byte indicator;
+    private int airportId;
+    private byte indicator;
 
     @Override
     public int compareTo(KeyWritableComparable keyWritableComparable) {
+        if (airportId > keyWritableComparable.airportId) {
+            return 1;
+        } else if (airportId == keyWritableComparable.airportId) {
+            if (indicator)
+        }
         return 0;
     }
 
