82576c61fc3f7c56b21bd26c45937a1dff48a98d
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:37:28 +0300

    Scripted auto-commit on change (2019-11-02 02:37:28) by gitwatch.sh

--- src/main/java/ReduceSideJoin.java
+++ src/main/java/ReduceSideJoin.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
@@ -10,5 +11,7 @@ public class ReduceSideJoin {
         job.setJobName("ReduceSideJoin");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsMapper.class);
+
+        FileOutputFormat
     }
 }
