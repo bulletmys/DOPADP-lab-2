95c69fddf7be45d0cf2743aa3e4885f43cd5760f
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:24:08 +0300

    Scripted auto-commit on change (2019-11-02 02:24:08) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -2,12 +2,15 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class JoinReducer extends Reducer<KeyWritableComparable, Text, Text, Text> {
     @Override
     protected void reduce(KeyWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Text airportName = values.iterator().next();
-        Itrea
-        while ()
+        Iterator<Text> iterator = values.iterator();
+        while (iterator.hasNext()) {
+            Text
+        }
     }
 }
