8e18e6a2ba7638d879cd6aceb21838799cb2e2ea
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:40:16 +0300

    Scripted auto-commit on change (2019-11-02 02:40:16) by gitwatch.sh

--- src/main/java/ReduceSideJoin.java
+++ src/main/java/ReduceSideJoin.java
@@ -17,6 +17,6 @@ public class ReduceSideJoin {
         job.setPartitionerClass(AirportIdPartitioner.class);
         job.setGroupingComparatorClass(AirportIdGroupComparator.class);
         job.setReducerClass(JoinReducer.class);
-        job.setMapOutputKeyClass(Text.pa);
+        job.setMapOutputKeyClass(Textpair);
     }
 }
