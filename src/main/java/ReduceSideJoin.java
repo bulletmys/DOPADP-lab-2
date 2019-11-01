e92b07e9483697810e6b4b7c11ef5e26838cda6d
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:32:57 +0300

    Scripted auto-commit on change (2019-11-02 02:32:57) by gitwatch.sh

--- src/main/java/ReduceSideJoin.java
+++ src/main/java/ReduceSideJoin.java
@@ -4,5 +4,6 @@ public class ReduceSideJoin {
     public static void main(String[] args) throws Exception {
         Job job = Job.getInstance();
         job.setJarByClass(ReduceSideJoin.class);
+        job.setJobName();
     }
 }
