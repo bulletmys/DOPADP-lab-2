b24de4987256741c23d83cf08dd81dbccdef49dc
bulletmys <bulletmys@yandex.ru>
2019-11-02 01:23:52 +0300

    Scripted auto-commit on change (2019-11-02 01:23:52) by gitwatch.sh

--- src/main/java/CsvReader.java
+++ src/main/java/CsvReader.java
@@ -1,9 +1,5 @@
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
 
 public class CsvReader {
 
