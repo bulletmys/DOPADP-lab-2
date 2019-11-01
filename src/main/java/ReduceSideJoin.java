6ccf0c6cd32e2ea935a276179aaede61fff9ea42
bulletmys <bulletmys@yandex.ru>
2019-11-02 02:32:02 +0300

    Scripted auto-commit on change (2019-11-02 02:32:02) by gitwatch.sh

--- src/main/java/ReduceSideJoin.java
+++ src/main/java/ReduceSideJoin.java
@@ -3,6 +3,6 @@ import org.apache.hadoop.mapreduce.Job;
 public class ReduceSideJoin {
     public static void main(String[] args) throws Exception {
         Job job = Job.getInstance();
-        
+        job.s
     }
 }
